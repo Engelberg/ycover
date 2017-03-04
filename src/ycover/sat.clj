3e6bf0695f5e4af7290eb5cc876f29630367a8ab
Mark Engelberg <mark.engelberg@gmail.com>
2017-03-03 17:53:00 -0800

    SAT

---
+++ src/ycover/sat.clj
@@ -0,0 +1,59 @@
+(ns ycover.sat
+    (:refer-clojure :exclude [cond])
+    (:require [better-cond.core :refer [cond]]
+              [clojure.set :refer [union]]
+              [ycover.core :refer [all-possible-Y-placements all-possible-Y-placements-10 all-possible-Y-placements-15]]
+              [rolling-stones.core :as sat :refer [! NOT AND OR XOR IFF IMP NOR NAND at-least at-most exactly]]))
+
+(defn build-constraints [placements]
+  (for [[row col] (apply union placements)
+        :let [cell [row col]]]
+    (exactly 1 (for [placement placements
+                     :when (contains? placement cell)]
+                 placement))))
+
+(def Y-constraints-10 (build-constraints all-possible-Y-placements-10))
+(def Y-constraints-15 (build-constraints all-possible-Y-placements-15))
+
+;(time (sat/true-symbolic-variables (sat/solve-symbolic-formula Y-constraints-10)))
+;(time (count (sat/solutions-symbolic-formula Y-constraints-10)))
+
+; Exploring double cells
+
+(defn build-constraints-double-cell [placements double-cells]
+  (for [[row col] (apply union placements)
+        :let [cell [row col]]]
+    (if (contains? double-cells cell)
+      (exactly 2 (for [placement placements
+                       :when (contains? placement cell)]
+                   placement))
+      (exactly 1 (for [placement placements
+                       :when (contains? placement cell)]
+                   placement)))))
+
+;(sat/true-symbolic-variables (sat/solve-symbolic-formula (build-constraints-double-cell (all-possible-Y-placements 4) #{[0 0] [3 3] [0 3] [3 0]})))
+
+;; Exploring symmetries
+
+(defn rotate-piece [placement board-size]
+  (set (for [[row col] placement]
+         [(- board-size row 1) (- board-size col 1)])))
+
+(defn reflect-piece [placement board-size]
+  (set (for [[row col] placement]
+         [row (- board-size col 1)])))
+
+(defn enforce-symmetries [placements board-size]
+  (for [placement placements]
+    (IFF placement (rotate-piece placement board-size))))
+
+(def symmetric-Y-constraints-10 (concat Y-constraints-10 (enforce-symmetries all-possible-Y-placements-10 10)))
+(def symmetric-Y-constraints-15 (concat Y-constraints-15 (enforce-symmetries all-possible-Y-placements-15 15)))
+;(time (sat/true-symbolic-variables (sat/solve-symbolic-formula symmetric-Y-constraints-10)))
+;(time (count (sat/solutions-symbolic-formula symmetric-Y-constraints-10)))
+
+(defn enforce-two-symmetries [placements board-size]
+  [(exactly 10 (for [placement placements]
+                 (AND placement (IFF placement (reflect-piece placement board-size)))))])
+(def two-symmetric-Y-constraints-10 (concat Y-constraints-10 (enforce-two-symmetries all-possible-Y-placements-10 10)))
+(def two-symmetric-Y-constraints-15 (concat Y-constraints-15 (enforce-two-symmetries all-possible-Y-placements-15 15)))
